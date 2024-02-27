b245e0603f94088db0168d03a1de390b2102c87e
nghiadv09 <nghia1392001@gmail.com>
2024-02-28 00:01:06 +0700

    feat: add analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import Footer from '@/components/Footer'
 import Contact from '@/components/Contact'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
 
@@ -19,6 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Analytics/>
         <Navbar />
         <Banner />
         <About />
